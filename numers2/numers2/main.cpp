47bea52a9a1744bb5b7a8011b7784661f91c00a5
joserm45 <joserodriguez@escolatecnos.cat>
2016-02-18 17:49:15 +0100

    Revert "Revert "Numbers""
    This reverts commit 010aab33978593ac91737628761f8710a51474ad.

---
+++ numers2/numers2/main.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+	int num1 = 0, num2 = 0,num3=0,num4=0, sol = 0, mult = 0, cont = 1,num=0,i=0;
+	printf("Dame el primer numero entero:");
+	scanf_s("%i", &num1);
+	printf("Dame el primer numero entero:");
+	scanf_s("%i", &num2);
+	printf("Dame el primer numero entero:");
+	scanf_s("%i", &num3);
+	printf("Dame el primer numero entero:");
+	scanf_s("%i", &num4);
+	for (i = 1; i<=10; i++) {
+		sol = num[i];
+		sol -= num[i];
+		
+	}
+
+	system("pause");
+	return 0;
+}
